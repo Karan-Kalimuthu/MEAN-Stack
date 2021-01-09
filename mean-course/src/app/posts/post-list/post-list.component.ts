cfae6fc151a3d517fdfef4c3e9c43e49798d8488
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-09 22:45:07 +0530

    Diving Into Structural Directives
    Diving Into Structural Directives

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -12,10 +12,10 @@ export class PostListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  posts = [
-    { title: 'First Posts', content: 'This is the first post content' },
-    { title: 'Second Posts', content: 'This is the second post content' },
-    { title: 'Third Posts', content: 'This is the third post content' }
-  ];
-
+  // posts = [
+  //   { title: 'First Posts', content: 'This is the first post content' },
+  //   { title: 'Second Posts', content: 'This is the second post content' },
+  //   { title: 'Third Posts', content: 'This is the third post content' }
+  // ];
+  posts = [];
 }
