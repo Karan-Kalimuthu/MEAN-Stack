efadbe0487fa02074efab1949a95c83404060c42
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-09 23:34:57 +0530

    Adding Forms
    Adding Forms

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
 
@@ -14,8 +15,11 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post: Post = { title: this.enteredTitle, content: this.enteredContent };
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = { title: form.value.title, content: form.value.content };
     this.postCreated.emit(post);
   }
   ngOnInit(): void {
