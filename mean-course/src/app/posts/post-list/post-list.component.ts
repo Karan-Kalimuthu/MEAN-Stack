36cd41255aac1e3efa77c8a4914f259e56d18dce
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-09 22:14:56 +0530

    Outputting Posts
    Outputting Posts

---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  posts = [
+    { title: 'First Posts', content: 'This is the first post content' },
+    { title: 'Second Posts', content: 'This is the second post content' },
+    { title: 'Third Posts', content: 'This is the third post content' }
+  ];
+
+}
