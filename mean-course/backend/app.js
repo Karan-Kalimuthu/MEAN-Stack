1b3068e663376a1f19ca37a50b9220b697e55f82
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-11 00:15:07 +0530

    etching Initial Posts
    etching Initial Posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
-});
-
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'fsjkbfjksbf', title: 'First Server Post', content: 'This is coming from the server' },
+    { id: 'dkfndsngkjs', title: 'Second Server Post', content: 'This is coming from the server!' },
+    { id: 'dklnkndfkgn', title: 'Third Server Post', content: 'This is coming from the server!!' },
+  ]
+  res.status(200).json({ message: 'Posts fetched successfully!', posts: posts });
 });
 
 module.exports = app;
