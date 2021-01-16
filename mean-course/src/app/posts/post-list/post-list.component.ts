1403abdc6754583cd3d0c619c314ca744a442a46
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-16 19:31:26 +0530

    Adding the Pagination Component
    Adding the Pagination Component

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
@@ -13,6 +14,9 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 5;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) { }
@@ -26,6 +30,11 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.posts = posts;
       });
   }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
+  }
+
   onDelete(postId: string) {
     this.postsService.deletePost(postId);
   }
