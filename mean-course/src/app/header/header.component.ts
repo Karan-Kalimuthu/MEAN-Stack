f224110b357eadf16ab1ed85bfbe21048a854380
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-17 21:53:10 +0530

    Improving the UI Header to Reflect the Authentication Status
    Improving the UI Header to Reflect the
    Authentication Status

--- mean-course/src/app/header/header.component.ts
+++ mean-course/src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListenerSub: Subscription
+  constructor(private authService: AuthService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.authListenerSub = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+  ngOnDestroy() {
+    this.authListenerSub.unsubscribe();
   }
 
 }
