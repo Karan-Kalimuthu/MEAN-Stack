10c196c5b4d1c5a8521785820846db6a0fa33c25
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-20 13:29:10 +0530

    The Error Interceptor
    The Error Interceptor

---
+++ mean-course/src/app/error-interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export class ErrorInterceptor implements HttpInterceptor {
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        alert(error.error.message);
+        return throwError(error);
+      })
+    );
+  }
+}
