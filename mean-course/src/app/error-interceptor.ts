ecf30dcda4151a37b644e3e91acf659b572e5e17
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-20 13:35:38 +0530

    Displaying the Basic Error Dialog
    Displaying the Basic Error Dialog

--- mean-course/src/app/error-interceptor.ts
+++ mean-course/src/app/error-interceptor.ts
@@ -1,14 +1,19 @@
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+import { ErrorComponent } from './error/error.component';
+
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+  constructor(private dialog: MatDialog) { }
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.message);
+        this.dialog.open(ErrorComponent)
         return throwError(error);
       })
     );
