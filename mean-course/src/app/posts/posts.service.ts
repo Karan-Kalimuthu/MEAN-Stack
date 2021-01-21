0bcb4a480ba2933f5012aefa2528f09ee3064700
Karan Kalimuthu <karan.k@iqzsystems.com>
2021-01-21 21:23:38 +0530

    Using a Global Angular Config
    Using a Global Angular Config

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -5,6 +5,9 @@ import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators'
 
 import { Post } from './post.model';
+import { environment } from '../../environments/environment';
+
+const BACKEND_URL = environment.apiUrl + "/posts/";
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +20,7 @@ export class PostsService {
   getPosts(postPerPage: number, currentPage: number) {
     const queryParams = `?pagesize=${postPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string, posts: any, maxPosts: number }>('http://localhost:3000/api/posts' + queryParams)
+      .get<{ message: string, posts: any, maxPosts: number }>(BACKEND_URL + queryParams)
       .pipe(map((postData) => {
         return {
           posts: postData.posts.map(post => {
@@ -42,7 +45,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string, title: string, content: string, imagePath: string, creator: string }>("http://localhost:3000/api/posts/" + id);
+    return this.http.get<{ _id: string, title: string, content: string, imagePath: string, creator: string }>(BACKEND_URL + id);
   }
 
   addPost(title: string, content: string, image: File) {
@@ -51,7 +54,7 @@ export class PostsService {
     postData.append("title", title);
     postData.append("content", content);
     postData.append("image", image, title);
-    this.http.post<{ message: string, post: Post }>('http://localhost:3000/api/posts', postData)
+    this.http.post<{ message: string, post: Post }>(BACKEND_URL, postData)
       .subscribe((responseData) => {
         // const post: Post = { id: responseData.post.id, title: title, content: content, imagePath: responseData.post.imagePath }
         // this.posts.push(post);
@@ -72,7 +75,7 @@ export class PostsService {
       postData = { id: id, title: title, content: content, imagePath: image, creator: null }
     }
     this.http
-      .put("http://localhost:3000/api/posts/" + id, postData)
+      .put(BACKEND_URL + id, postData)
       .subscribe(response => {
         // const updatedPosts = [...this.posts];
         // const oldPostIndex = updatedPosts.findIndex((p) => p.id === id);
@@ -85,7 +88,7 @@ export class PostsService {
   };
 
   deletePost(postId: string) {
-    return this.http.delete("http://localhost:3000/api/posts/" + postId);
+    return this.http.delete(BACKEND_URL + postId);
     // .subscribe(() => {
     //   console.log('Deleted');
     //   const updatedPosts = this.posts.filter(post => post.id !== postId);
